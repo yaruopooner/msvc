11613fbda22c2c7fb3904570580bcb157861110a
yaruopooner <yaruopooner@gmail.com>
2014-07-09 03:15:36 +0900

    update minimal sample code

--- minimal-config-sample/init.el
+++ minimal-config-sample/init.el
@@ -1,5 +1,5 @@
 ;;; -*- Mode: Emacs-Lisp ; Coding: utf-8-unix -*-
-;;; last updated : 2014/03/07.13:16:58
+;;; last updated : 2014/07/09.03:10:51
 
 
 ;;==============================================================================
@@ -15,11 +15,42 @@
 (setq shell-command-switch "-c")
 
 
+(require 'package)
+
+(add-to-list 'package-archives '("melpa"		.		"http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("marmalade"	.		"http://marmalade-repo.org/packages/"))
+
+(package-initialize)
+
+
+(defun package-installer (packages)
+  (let (targets)
+	(dolist (name packages)
+	  (unless (package-installed-p name)
+		(message "Package: %s : not installed. " name)
+		(add-to-list 'targets name t)))
+
+	(when targets
+	  (package-refresh-contents)
+
+	  (dolist (name targets)
+		(package-install name)))))
+
+
+(defconst msvc-dependency-package-list '(auto-complete
+										 fuzzy
+										 yasnippet
+										 dropdown-list))
+
+
+(package-installer msvc-dependency-package-list)
+
+
 ;; built-in packages
 (require 'cedet.config)
 (require 'flymake.config)
 
-;; plug-in packages
+;; dependency packages
 (require 'setup.auto-complete)
 (require 'setup.yasnippet)
 
